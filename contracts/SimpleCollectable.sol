c72e88c14ad1ad00ebe9d1d5bf991e4d796f0a39
dennis-mureti <dennismureti47@gmail.com>
2022-10-22 20:13:04 +0300

    create NFT

--- contracts/SimpleCollectable.sol
+++ contracts/SimpleCollectable.sol
@@ -3,3 +3,17 @@
 pragma solidity 0.6.6;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract SimpleCollectable is ERC721 {
+    uint256 public tokenCounter;
+    constructor () public ERC721 ("Dogie", "DOG"){
+        tokenCounter = 0;
+    }
+
+    function createCollectable() pulic returns (uint256){
+        uint256 newTokenId = tokenCounter;
+        _safeMint(msg.sender, newTokenId);
+        tokenCounter = tokenCounter + 1;
+        return newTokenId;
+    }
+}
